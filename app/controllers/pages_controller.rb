a3653a1d7ce13c36bce9933b22b56003ae5d949f
vovanmozg <vovanmozg@gmail.com>
2023-04-21 18:35:12 +0400

    Update to Rails 7

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def page
+    url = url_for(lang: params[:lang], country: params[:country])
     render json: {
       params: params,
-      url_for: url_for(lang: params[:lang], country: params[:country])
+      url_for: url
     }
   end
 end
